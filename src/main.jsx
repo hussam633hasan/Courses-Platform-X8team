10bbcbb39edd55a8ba48a6c1c350c3aa87c080f4
unknown <kotaybaehatem@gmail.com>
2024-06-10 01:28:25 -0400

    footerComponentEdit

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
->>>>>>> aac04807ea4f8eb040305bdf209f1cc69d81476c
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<< HEAD
     <App />
   </React.StrictMode>,
 )
 // src/main.jsx أو src/index.jsx
 
-=======
-    <Browser.Router>
-      <App />
-    </Browser.Router>
-  </React.StrictMode>
-);
->>>>>>> aac04807ea4f8eb040305bdf209f1cc69d81476c
--- src/pages/Article/Article.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Article() {
-    return( <>
-        </>
-        )
-}
-export default Aritcle;
