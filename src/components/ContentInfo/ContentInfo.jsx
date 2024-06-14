481811fffd4a58b3c5a68623cbb600d752cfd55e
Ezis <ezisissa@gmail.com>
2024-06-14 21:53:33 +0200

    What you will learn section

--- src/components/ContentInfo/ContentInfo.jsx
+++ src/components/ContentInfo/ContentInfo.jsx
@@ -5,22 +5,22 @@ function ContentInfo() {
     return( 
     <section className="content">
         <div className="platformInfo">
-            <h1>What You will learn</h1>
+            <h2>What You will learn</h2>
             <p>Up-to-date knowledge from recognized market experts for beginners and practitioners</p>
             <p>On the platform, you can gain knowledge on current topics and
             in-demand skills. All courses are aimed at practice: we monitor the relevance of the material and help with employment and internship</p>
         </div>
         <div className="statistics">
             <div className="stat1">
-                <h1>56</h1>
+                <h2>56</h2>
                 <p>Courses</p>
             </div>
             <div className="stat2">
-                <h1>108</h1>
+                <h2>108</h2>
                 <p>Creators</p>
             </div>
             <div className="stat3">
-                <h1>3 723</h1>
+                <h2>3 723</h2>
                 <p>Graduates</p>
             </div>
         </div>
