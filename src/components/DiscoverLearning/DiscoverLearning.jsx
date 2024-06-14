74c2a29b04f086e4c81810bf1b0203a81ad6ab42
Dema Jafar <dema.jaf94@gmail.com>
2024-06-14 12:39:21 +0300

    first commit

---
+++ src/components/DiscoverLearning/DiscoverLearning.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./DiscoverLearning.css"
+import { Container } from 'react-bootstrap'
+
+export default function DiscoverLearning() {
+    return (
+        <Container className='py-5 DJ-discover'>
+            <h2>Discover Lifelong Learning</h2>
+        </Container>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import DiscoverLearning from "../../components/DiscoverLearning/DiscoverLearning";
 
 function Home() {
   return( <>
+  <DiscoverLearning />
   </>
   )
 }
