b1d3d6db7a5690aa05a2ae03475db52cb239d909
Ezis <ezisissa@gmail.com>
2024-06-15 11:35:31 +0200

    Promotion

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./../../components/Navbar/Navbar.jsx"
 import ContentInfo from "./../../components/ContentInfo/ContentInfo.jsx"
 import Promotion from "./../../components/Promotion/Promotion.jsx"
-import Footer from "./../../components/Footer/Footer.jsx"
 
 function Home() {
   
