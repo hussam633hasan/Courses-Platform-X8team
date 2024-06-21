39a0a1a0eeb4f02f314608a898e28312d9eb2ac1
Hussam Hasan <163654192+hussam633hasan@users.noreply.github.com>
2024-06-21 15:47:50 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 import DiscoverLearning from "../../components/DiscoverLearning/DiscoverLearning";
 import WhatWillDiscover from "../../components/WhatWillDiscover/WhatWillDiscover";
+import ContentInfo from "./../../components/ContentInfo/ContentInfo.jsx";
+import Promotion from "./../../components/Promotion/Promotion.jsx";
+import Process from "./../../components/Process/Process.jsx"
 
 function Home() {
   return( <>
   <DiscoverLearning />
   <WhatWillDiscover />
+  <ContentInfo />
+  <Process />
+  <Promotion />
   </>
   )
 
