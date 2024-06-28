6e955a3f7b3b0b4e5567461a3e37b15ff299534c
bayankak <noumaali117@gmail.com>
2024-06-28 23:34:15 +0300

    update

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,13 +1,12 @@
 
 import React from "react";
-<<<<<<< HEAD
+
 import HeaderCourses from "../../components/HeaderCourses/HeaderCourses.jsx";
-import StudentsProjects from '../../components/StudentsProjects/StudentsProjects';
-=======
+
 import ProfessionDesigner from "../../components/ProfessionDesigner/ProfessionDesigner";
 
-import StudentProjects from '../../components/StudentsProjects/StudentsProjects';
->>>>>>> 30dbbb7835c885adb8922d5bd872ea8f9c91d8ee
+import StudentsProjects from '../../components/StudentsProjects/StudentsProjects';
+
 import TrainingProgram from '../../components/TrainingProgram/TrainingProgram';
 import CoursesInfo from "./../../components/CoursesInfo/CoursesInfo.jsx"
 import Programs from "../../components/Programs/Programs.jsx";
