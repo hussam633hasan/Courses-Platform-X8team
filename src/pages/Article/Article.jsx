5a6b39399fe3a3f3cdf4040517ca4c87f621eaa0
bayankak <noumaali117@gmail.com>
2024-07-01 18:18:39 +0300

    update

--- src/pages/Article/Article.jsx
+++ src/pages/Article/Article.jsx
@@ -5,16 +5,16 @@ import StylesTop from '../../components/StylesTop/StylesTop';
 
 
 function Article() {
-<<<<<<< HEAD
+
     return( <>
     <StylesTop />
  
 </>
        
         )
-=======
+
   return <></>;
 
->>>>>>> 672c6efde3519ffd2e163687a7628d1b86e26b60
+
 }
 export default Article;
