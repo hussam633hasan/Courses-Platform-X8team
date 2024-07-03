2a7c74f628d002ec82c367bb66aea3ef411866f9
hussam633hasan <husamhasan633@gmail.com>
2024-07-03 22:03:46 +0300

    up

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Courses-Platform-X8team">
       <App />
     </BrowserRouter>
   </React.StrictMode>
